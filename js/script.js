d4e56d0876f1404be464918988d30b5da84a3579
Matthew Aitken <matt@itken.net>
2024-08-06 15:51:57 +0000

    Add function to generate a set of random non repeating questions from the pool no more than the total pool

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // BUILD THE QUESTIONS POOL
-let questions = [
+let questionPool = [
     {
         question: "Who is the father of Rhaenyra Targaryen?",
         answer: "Viserys I Targaryen",
@@ -42,3 +42,26 @@ let questions = [
         incorrect: ["Rhaenyra Targaryen","Dragons are cool","Laena Velaryon","Rhaenys Targaryen"],
     }
 ]
+
+/**
+ * Generates a random array for the round from the question pool
+ * @param {} amount how many questions the round will be
+ * @returns an array of numbers used to pull questions from the pool
+ */
+function generateQuestions(amount) {
+    let set = [];  
+    while (set.length < amount && set.length < questionPool.length) {
+      let num = (Math.floor(Math.random()*10));
+      if (!set.includes(num)) {
+          set.push(num);
+      }
+  }
+  return set;
+}
+
+// determine questions for the round
+let questionSet = generateQuestions(5);
+
+for (question of questionSet) {
+    console.log(questionPool[question].question);
+}
