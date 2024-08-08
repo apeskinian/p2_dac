9b88e1dcd0943180ba6f70ffee38541538d8bb71
Matthew Aitken <matt@itken.net>
2024-08-08 13:41:29 +0000

    Show answer to question and let user know if they got it right by message and highlighting the correct and if chosen the incorrect answer

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -6,6 +6,7 @@ let playerScore = 0;
 let dragonScore = 0;
 let noSelectionMessage = document.getElementById('no-selection');
 let answerOptions = document.getElementsByName('attempt');
+let answerBoxes = document.getElementsByClassName('question-box');
 
 //FUNCTIONS
 
@@ -53,7 +54,7 @@ function loadQuestion(n) {
   document.getElementById('question').innerText = nextQuestion.question;
   let allAnswers = ([...nextQuestion.wrongAnswers, nextQuestion.correctAnswer]);
   let shuffledAnswers = shuffle(allAnswers);
-  let answerBoxes = document.getElementsByClassName('question-box');
+  // let answerBoxes = document.getElementsByClassName('question-box');
   for (let n = 0;  n <= 4; n++) {
     answerBoxes[n].innerText = shuffledAnswers[n];
     answerOptions[n].value = shuffledAnswers[n];
@@ -75,9 +76,14 @@ console.log('Loading first question...');
 loadQuestion(currentQuestion);
 
 function nextQuestionPlease() {
-  //UNCHECK ANSWERBOXES
-  for (let uncheck of answerOptions) {
-    uncheck.checked = false;
+  //REVERT ANSWERBOXES AND RESET SUBMIT BUTTON NAME
+  document.getElementById('submit-button').textContent = 'Dracarys!';
+  for (let option of answerOptions) {
+    option.disabled = false;
+  }
+  for (let box of answerBoxes) {
+    box.classList.remove('correct-answer');
+    box.classList.remove('incorrect-answer');
   }
   if (currentQuestion < numberOfQuestions) {
     loadQuestion(currentQuestion);
@@ -89,6 +95,40 @@ function nextQuestionPlease() {
   }
 }
 
+function highlightCorrectAnswer(correct, given) {
+  //UNCHECK ALL BOXES SO THAT BACKROUND COLOUR IS SEEN AND DISABLE INPUTS
+  for (let uncheck of answerOptions) {
+    uncheck.checked = false;
+    uncheck.disabled = true;
+  }
+  for (let label of answerBoxes) {
+    if (label.innerText === correct) {
+      label.classList.add('correct-answer');
+    }
+  }
+  if (given !== correct) {
+    for (let label of answerBoxes) {
+      if (label.innerText === given) {
+        label.classList.add('incorrect-answer');
+      }
+    }
+  }
+}
+
+function correctAnswerGiven(answer) {
+  playerScore += 1;
+  document.getElementById('question').innerText = 'Well done, wise one! You chose correctly, march on!';
+  document.getElementById('submit-button').textContent = 'Raise the banners!';
+}
+
+function incorrectAnswerGiven(answer) {
+  if (answer === "Dragons are cool") {
+    dragonScore += 1;
+  }
+  document.getElementById('question').innerText = 'Alas, that was not the right choice. Keep moving on, brave soul!';
+  document.getElementById('submit-button').textContent = 'Seal my fate!';
+}
+
 function answerSubmitted() {
   //CHECK IF SUBMIT ANSWER FOR CHECKING OR NEXT QUESTION REQUEST
   if (document.getElementById('submit-button').textContent === "Dracarys!") {
@@ -97,18 +137,20 @@ function answerSubmitted() {
     for (let answer of answerOptions) {
       if (answer.checked) {
         givenAnswer = answer.value;
-        console.log(givenAnswer);
       }
     }
-    if (givenAnswer === "")
-     noSelectionMessage.style.display = "flex";
-    //CHECK IF ANSWER IS CORRECT, IF SO INCREASE SCORE
+    if (givenAnswer === "") {
+      noSelectionMessage.style.display = "flex";
+    }
+    //CHECK IF ANSWER IS CORRECT AND CALL RELEVANT FUNCTION
     let actualAnswer = questionPool[questionSet[currentQuestion]].correctAnswer;
     if (givenAnswer === actualAnswer) {
-     //DO CORRECT ANSWER FUNCTION
+      correctAnswerGiven(givenAnswer);
     } else {
-      //DO INCORRECT ANSWER PLUS DAC CHECK 
+      incorrectAnswerGiven(givenAnswer);
     }
+    //HIGHLIGHT CORRECT ANSWER
+    highlightCorrectAnswer(actualAnswer, givenAnswer);
   } else {
     currentQuestion += 1;
     nextQuestionPlease();
