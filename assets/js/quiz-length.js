971cf0eb07aa97a95933f1433643857fc46f176a
Matthew Aitken <matt@itken.net>
2024-08-12 21:32:01 +0000

    Remove inline js from index.html and replace with eventlistener to quiz-length.js

--- assets/js/quiz-length.js
+++ assets/js/quiz-length.js
@@ -1,15 +1,14 @@
 /*jshint esversion: 11 */
 
-/**
- * Save the user quiz length choice to local storage and begin the game.
- */
-function startQuiz() {
-    let lengthOptions = document.getElementsByName('quiz-length');
-    for (let option of lengthOptions) {
-        if (option.checked) {
-            localStorage.setItem('questions', option.id);
-        }
-    }
-    window.location.href="game.html";
-}
+const startButton = document.getElementById('start-quiz-button');
 
+// Save the user quiz length choice to local storage and begin the game.
+startButton.addEventListener('click', function() {
+    let lengthOptions = document.getElementsByName('quiz-length');
+        for (let option of lengthOptions) {
+            if (option.checked) {
+                localStorage.setItem('questions', option.id);
+            }
+        }
+    window.location.href="game.html";
+})
